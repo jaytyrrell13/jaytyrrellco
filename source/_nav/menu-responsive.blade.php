fba9e553756a51611255abfd85f39ba6b037756b
Jay Tyrrell <tyrrell.jay@gmail.com>
2020-05-16 20:49:56 -0400

    Remove contact page

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -21,12 +21,5 @@
                 class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/uses') ? 'active text-blue' : '' }}"
             >Uses</a>
         </li>
-        <li class="pl-4">
-            <a
-                title="Contact Page"
-                href="/contact"
-                class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/contact') ? 'active text-blue' : '' }}"
-            >Contact</a>
-        </li>
     </ul>
 </nav>
--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -2,5 +2,4 @@
     <a title="Blog Page" href="/blog" class="ml-6 border-0 text-gray-600 font-bold">Blog</a>
     <a title="About Page" href="/about" class="ml-6 border-0 text-gray-600 font-bold">About</a>
     <a title="Uses Page" href="/uses" class="ml-6 border-0 text-gray-600 font-bold">Uses</a>
-    <a title="Contact Page" href="/contact" class="ml-6 border-0 text-gray-600 font-bold">Contact</a>
 </nav>
