edcd54b84fdfb38ebba7987458a933ee99d81c44
Jay Tyrrell <tyrrell.jay@gmail.com>
2020-07-11 14:01:15 -0400

    Update uses page

--- source/uses.blade.php
+++ source/uses.blade.php
@@ -10,9 +10,30 @@ title: Uses
 @section('body')
     <p>After getting inspired from <a href="https://uses.tech/" target="_blank">Uses.tech</a>, here's a list of the tech I use regularly.</p>
 
+    <ul class="mt-3">
+        <li><strong>Computer</strong>: 15" MacBook Pro
+            <ul>
+                <li>Early 2013</li>
+                <li>2.4 GHz Quad-Core Intel Core i7</li>
+                <li>8 GB 1600 MHz DDR3</li>
+                <li>256 GB Storage</li>
+            </ul>
+        </li>
+        <li><strong>Desk</strong>: <a href="https://www.bushbusinessfurniture.com/OurFurniture/ShopbyCollection/BushBusinessFurnitureCollections/SeriesC.aspx?pf=140&ProductSKU=SRC071MASU&Finish=Mahogany">Bush Business Furniture Series C</a></li>
+        <li><strong>Headphones</strong>: <a href="https://www.sony.com/electronics/headband-headphones/wh-1000xm3" target="_blank">Sony WH-1000XM3</a></li>
+        <li><strong>Monitor</strong>: <a href="https://www.vizio.com/e320fib2.html">Vizio E320fi-B2</a></li>
+        <li><strong>Phone</strong>: <a href="https://www.apple.com/iphone-xr/specs/" target="_blank">iPhone XR</a>
+            <ul>
+                <li>64 GB Storage</li>
+            </ul>
+        </li>
+        <li><strong>Watch</strong>: <a href="https://www.apple.com/apple-watch-series-3/" target="_blank">Apple Watch Series 3</a></li>
+    </ul>
+
     <h4 class="mt-8 mb-0">Software</h4>
     <ul class="mt-3">
         <li><a href="https://www.alfredapp.com/" target="_blank">Alfred</a></li>
+        <li><a href="https://duckduckgo.com/" target="_blank">DuckDuckGo</a></li>
         <li><a href="https://github.com/tonsky/FiraCode" target="_blank">Fira Code</a> font</li>
         <li><a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank">Firefox</a></li>
         <li><a href="https://iterm2.com/" target="_blank">iTerm</a> with ZSH and <a href="https://github.com/hwyncho/ayu-iTerm" target="_blank">Ayu Mirage</a> theme</li>
@@ -22,14 +43,7 @@ title: Uses
         <li><a href="https://www.sublimemerge.com/" target="_blank">Sublime Merge</a></li>
         <li><a href="https://www.sublimetext.com/3" target="_blank">Sublime Text 3</a> with <a href="https://github.com/dempfi/ayu" target="_blank">Ayu Mirage</a> theme</li>
         <li><a href="https://apps.apple.com/us/app/unsplash-wallpapers/id1284863847?mt=12" target="_blank">Unsplash Wallpapers</a></li>
+        <li><a href="https://matthewpalmer.net/vanilla/" target="_blank">Vanilla</a></li>
         <li><a href="https://www.youneedabudget.com/" target="_blank">You Need A Budget</a></li>
-    </ul>
-
-    <h4 class="mt-8 mb-0">Hardware</h4>
-    <ul class="mt-3">
-        <li>15" MacBook Pro (Early 2013, 2.4 GHz Quad-Core Intel Core i7, 8 GB 1600 MHz DDR3)</li>
-        <li>iPhone XR</li>
-        <li>Looking to buy an external monitor soon</li>
-        <li><a href="https://www.sony.com/electronics/headband-headphones/wh-1000xm3" target="_blank">Sony WH-1000XM3</a> Headphones</li>
     </ul>
 @endsection
