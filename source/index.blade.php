bf26d86cdad81c4c1c4659e4c3d8c158aa855fce
Jay Tyrrell <tyrrell.jay@gmail.com>
2021-02-08 21:36:34 -0500

    Show latest articles on index page

--- source/index.blade.php
+++ source/index.blade.php
@@ -18,7 +18,7 @@
         </div>
     </div>
 
-    {{-- <h3 class="mb-6">Latest Articles</h3>
+    <h3 class="mt-12 mb-6">Latest Articles</h3>
 
     @foreach ($posts->take(6)->chunk(2) as $row)
         @foreach ($row as $post)
@@ -32,5 +32,5 @@
         @if (! $loop->last)
             <hr class="w-full border-b mt-2 mb-6">
         @endif
-    @endforeach --}}
+    @endforeach
 @stop
