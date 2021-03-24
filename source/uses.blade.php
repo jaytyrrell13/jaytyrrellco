d97bc1175f7a6b53ba79236228dfa04bc031e43a
Jay Tyrrell <tyrrell.jay@gmail.com>
2021-03-24 18:52:41 -0400

    Update uses page

--- source/uses.blade.php
+++ source/uses.blade.php
@@ -10,15 +10,16 @@ title: Uses
 @section('body')
     <p>After getting inspired from <a href="https://uses.tech/" target="_blank">Uses.tech</a>, here's a list of the tech I use regularly.</p>
 
+    <p><strong>Last updated</strong>: {{ date('m/d/Y') }}</p>
+
     <ul class="mt-3">
         <li><strong>Desk</strong>: <a href="https://www.bushbusinessfurniture.com/OurFurniture/ShopbyCollection/BushBusinessFurnitureCollections/SeriesC.aspx?pf=140&ProductSKU=SRC071MASU&Finish=Mahogany" target="_blank">Bush Business Furniture Series C</a></li>
         <li><strong>Headphones</strong>: <a href="https://www.sony.com/electronics/headband-headphones/wh-1000xm3" target="_blank">Sony WH-1000XM3</a></li>
         <li><strong>Keyboard</strong>: <a href="https://www.amazon.com/gp/product/B071ZZTNBM/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1" target="_blank">Apple Magic Keyboard with Numeric Keypad</a></li>
-        <li><strong>Laptop</strong>: 15" MacBook Pro
+        <li><strong>Laptop</strong>: 13" MacBook Air
             <ul>
-                <li>Early 2013</li>
-                <li>2.4 GHz Quad-Core Intel Core i7</li>
-                <li>8 GB 1600 MHz DDR3</li>
+                <li>M1 2020</li>
+                <li>16 GB Memory</li>
                 <li>256 GB Storage</li>
             </ul>
         </li>
@@ -45,9 +46,9 @@ title: Uses
     <h4 class="mt-8 mb-0">Software</h4>
     <ul class="mt-3">
         <li><a href="https://www.alfredapp.com/" target="_blank">Alfred</a></li>
+        <li><a href="https://brave.com/" target="_blank">Brave Browser</a></li>
         <li><a href="https://duckduckgo.com/" target="_blank">DuckDuckGo</a></li>
         <li><a href="https://github.com/tonsky/FiraCode" target="_blank">Fira Code</a> font</li>
-        <li><a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank">Firefox</a></li>
         <li><a href="https://iterm2.com/" target="_blank">iTerm</a> with ZSH and <a href="https://github.com/hwyncho/ayu-iTerm" target="_blank">Ayu Mirage</a> theme</li>
         <li><a href="https://www.lastpass.com/" target="_blank">LastPass</a></li>
         <li><a href="https://www.notion.so/" target="_blank">Notion</a></li>
@@ -55,7 +56,6 @@ title: Uses
         <li><a href="https://www.sublimemerge.com/" target="_blank">Sublime Merge</a></li>
         <li><a href="https://www.sublimetext.com/3" target="_blank">Sublime Text 3</a> with <a href="https://github.com/dempfi/ayu" target="_blank">Ayu Mirage</a> theme</li>
         <li><a href="https://apps.apple.com/us/app/unsplash-wallpapers/id1284863847?mt=12" target="_blank">Unsplash Wallpapers</a></li>
-        <li><a href="https://matthewpalmer.net/vanilla/" target="_blank">Vanilla</a></li>
         <li><a href="https://www.youneedabudget.com/" target="_blank">You Need A Budget</a></li>
     </ul>
 @endsection
