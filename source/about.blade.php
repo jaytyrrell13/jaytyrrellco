79f12367a6321684f5add21886572099178241cf
Jay Tyrrell <tyrrell.jay@gmail.com>
2023-08-26 21:54:15 -0400

    Update about page

--- source/about.blade.php
+++ source/about.blade.php
@@ -8,7 +8,7 @@ title: About
 @endsection
 
 @section('body')
-    <p>Hey! I'm Jay Tyrrell. I was born and raised in South Florida and moved to NYC in 2012. Now, I am living in Bay Shore, NY with my wife.</p>
+    <p>Hey! I'm Jay Tyrrell. I was born and raised in South Florida and moved to NYC in 2012. Now, I am living in Bay Shore, NY with my wife and son.</p>
 
     <p>I currently work at <a href="https://www.ableto.com/" target="_blank">AbleTo</a>, a leading provider of virtual behavioral health care.</p>
 
@@ -22,18 +22,18 @@ title: About
 
     <ul class="mt-2">
         <li>PHP</li>
-        <li>Javascript</li>
-        <li>Node</li>
+        <li>Javascript/Typescript</li>
     </ul>
 
     <p class="mb-0"><strong>Libraries & Frameworks</strong></p>
 
     <ul class="mt-2">
+        <li><a href="https://expressjs.com/" target="_blank">ExpressJS</a></li>
         <li><a href="https://laravel.com/" target="_blank">Laravel</a></li>
+        <li><a href="https://react.dev/" target="_blank">React</a></li>
+        <li><a href="https://tailwindcss.com/" target="_blank">Tailwind CSS</a></li>
         <li><a href="https://vuejs.org/" target="_blank">VueJS</a></li>
         <li><a href="https://router.vuejs.org/" target="_blank">Vue Router</a></li>
         <li><a href="https://vuex.vuejs.org/" target="_blank">Vuex</a></li>
-        <li><a href="https://tailwindcss.com/" target="_blank">Tailwind CSS</a></li>
-        <li><a href="http://lesscss.org/" target="_blank">Less CSS</a></li>
     </ul>
 @endsection
